c2ba6d9a4a685d42aab6b9269ddc6131de6e84e7
catalin88 <catalin_t_vulcan@yahoo.com>
2017-02-04 21:20:50 +0200

    Remove params & order by id

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -1,16 +1,6 @@
 Spree::Admin::ReportsController.class_eval do
   def stock
-    params[:q] = { } unless params[:q]
-
-    if params[:q][:count_on_hand_gteq].blank?
-      params[:q][:count_on_hand_gteq] = -1000
-    end
-
-    if params[:q][:count_on_hand_lteq].blank?
-      params[:q][:count_on_hand_lteq] = 100000
-    end
-
-    @search = Spree::StockItem.joins(:variant, :stock_location).order('spree_variants.sku ASC').ransack(params[:q])
+    @search = Spree::StockItem.joins(:variant, :stock_location).order('spree_variants.id ASC').ransack()
     @stock_items = @search.result
   end
 end
